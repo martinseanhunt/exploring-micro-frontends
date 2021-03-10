03aa715486ba979ebf2ce3f4e28e64aa88e61f3d
Martin Hunt <martinseanhunt@gmail.com>
2021-03-10 13:39:44 +0000

    set up react

---
+++ container/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export function App() {
+  return (
+    <main>
+      <h1>Welcome to react</h1>
+    </main>
+  )
+}
--- container/src/renderApp.js
+++ container/src/renderApp.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import { App } from './App'
+
 // We don't need a mount function like in other FE's because the
 // container is never being run from within another FE.
-ReactDOM.render(<div>This is just a test</div>, document.querySelector('#root'))
+ReactDOM.render(<App />, document.querySelector('#root'))
